92900aacabcbbbe53fdaaf1ecb47275dedf3968e
Brian Fife <bdfife@gmail.com>
2013-11-25 18:32:59 -0500

    Updated handle_url to, well, work.

--- handle_url.py
+++ handle_url.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
-
 
 # These are the nethack characters that must be html-escaped
 escape_chars = {
@@ -38,7 +37,7 @@ if __name__ == '__main__':
         print("Usage: handle_url.py [turn number]")
     else:
         turn = sys.argv[1]
-        print("Processing data for turn %s" % turn)
+        #print("Processing data for turn %s" % turn)
         
         # import the screenshot data
         with open('%s.nss' % turn) as scr_file:
@@ -54,7 +53,7 @@ if __name__ == '__main__':
 
         # process the screenshot data
         for line in scr_data:
-            htm_data.append(escape(line))
+            htm_data.append(line[:-1].ljust(80, " "))
 
         # make sure the pre close tag is appended to the last line
         # then add the other closing tags
@@ -64,34 +63,56 @@ if __name__ == '__main__':
 
         # convert the list to a string in sections:
         # header, body, footer
-        htm_header = "\n".join(htm_data[0:3])
-        htm_body = "\n".join(htm_data[4:-2])
-        htm_footer = "\n".join(htm_data[-2:])
+        htm_header = "\n".join(htm_data[0:4])
+        htm_body = "\n".join(htm_data[4:-4])
+        htm_footer = "\n".join(htm_data[-4:])
    
         # add in the inventory and object descriptions
         with open('%s.inv' % turn) as inv_file:
             inv_data = inv_file.readlines()
         
-        with open('%s.inv' % turn) as obj_file: 
+        with open('%s.obj' % turn) as obj_file: 
             obj_data = obj_file.readlines()
 
         full_htm_data = ""
-        
+        full_htm_data += htm_header
+
+        #print("check 0")
+        #print(htm_body)
+        #print("check 1")
+        #print(full_htm_data)
+
         # naturally, this will have to be filled out soon!
-        obj_list = "@(+d"
+        obj_list = "@)+d"
         obj_counter = 0
-        
+
+        #print(htm_body)
+
         # replace an 'object of interest' with the marked up 
         # description text
         for char in htm_body:
             if char in obj_list:
-                print(char)
-                href_str = '''<a href=# class="det">%s<div>%s</div></a>''' % (char, obj_data[obj_counter])
+                if char in escape_chars:
+                    char = escape_chars[char]
+                # don't forget to snip the newline from obj_counter.
+                href_str = '''<a href=# class="det">%s<div>%s</div></a>''' \
+                              % (char, obj_data[obj_counter][:-1])
                 obj_counter += 1
                 full_htm_data += href_str
             else:
+                if char in escape_chars:
+                    char = escape_chars[char]
                 full_htm_data += char
 
-        # export the file
-        print(htm_data)
 
+        full_htm_data += "\n" + htm_footer
+
+        # fix out the [I] tag.
+        # note, it is concievable that '[I]' could occur organically
+        # on the game board. I'm ignoring that possiblity for now. 
+        # fixme: this is trivially addressed by only modifying htm_footer
+        
+        inv_block = "".join(inv_data)        
+        inv_htm_data = full_htm_data.replace(r'[I]',"""<a href=# class="inv">[I]<div>%s</div></a>""" % inv_block)
+        
+        print(inv_htm_data)
