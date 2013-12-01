d3d7f5b3c72b0ceaa58cbc89999921464bf2f124
Brian Fife <bdfife@gmail.com>
2013-12-01 09:42:45 -0500

    Starting merge of build_page.

--- build_page.py
+++ build_page.py
@@ -13,13 +13,16 @@ requires:
 [turn].str - story associated with turn
 [turn].tip - tip associated with turn
 
-usage: 'build_post.py [turn_number]'
+usage: 'build_post.py [delve_id] [turn_number]'
 
 should be called from the directory where the netHackDaily files are located.
 this script requires python3, because unicode. 
 """
 
 import sys
+import os
+from config import py as cfg
+from jinja2 import Environment, FileSystemLoader
 
 # These are the nethack characters that must be html-escaped
 escape_chars = {
@@ -38,41 +41,10 @@ escape_chars = {
     "┘": "&#9496;",
     }
 
-def build_header(turn, date):
-    
-    html_header = [
-        '\t\t\t<div class="netHackScr">',
-        '<!-- The following section must not be indented or it will mess' +
-        'up the <pre> rendering -->',
-        ]
+def build_nethack_section(nss, obj, inv):
+    # Takes in the data from [turn].nss.readlines()
 
-    return html_header
-
-def build_footer(story="story goes here", tip="tip goes here"):
-
-    html_footer = [
-        '\t\t\t</div> <!-- netHackScr -->',
-        '\t\t\t<div class="comments">',
-        '\t\t\t\t<div class="story">',
-        '\t\t\t\t\t<p>%s</p>'% story,
-        '\t\t\t\t</div> <!-- story -->',
-        '\t\t\t\t<div class="tip">',
-        '\t\t\t\t\t<p>%s</p>'% tip,
-        '\t\t\t\t</div> <!-- tip -->',
-        '\t\t\t</div> <!-- comments -->',
-        ]
-    
-    return html_footer
-
-def build_page(turn=1, date="January 1, 2013"):
-
-    # import the screenshot data
-    with open('%s.nss' % turn) as scr_file:
-        scr_data = scr_file.readlines()
-        # hack in my 'inventory' tag to the last line.
-        scr_data[-1] = scr_data[-1].rstrip('\n') + " [I]\n"
-
-    # Now build the netHack 'screenshot' as a <pre> section
+    # Builds the netHack 'screenshot' as a <pre> section
     # utf-8 shapes must be escaped and all lines must be padded to 80 chars
     # we are also drawing a box around the 'screenshot'
     #
@@ -80,8 +52,8 @@ def build_page(turn=1, date="January 1, 2013"):
     # the NetHack screen can be divided into three sections:
     #
     # The header section includes some text/status messages, and the 
-    # border. Except for the border, it will not contain characters that must
-    # be HTML-escaped.
+    # border. Except for the border, the header will not contain characters 
+    # that must be HTML-escaped.
     # 
     # The body section is where the 'map' and any items/monsters are drawn.
     # this section may contain HTML-unsafe characters like <, > *and* includes
@@ -89,17 +61,24 @@ def build_page(turn=1, date="January 1, 2013"):
     # 
     # The footer section includes the two attribute bars. We have also added a 
     # special character, [I], that is used to show the player's inventory. 
-    # Finally, the bottom border is also drawn. Except for the border, it will
-    # not contain characters that must be HTML-escaped. 
-    
-    # generate the 'fixed' entry HTML header code. 
-    # the only dynamic elements are turn # and date.
-    htm_data = build_header(turn=turn, date=date)
-    
+    # Finally, the bottom border is also drawn. Except for the border, the 
+    # footer will not contain characters that must be HTML-escaped. 
+
+    scr_data = nss
+
+    ###
+    # Draw the nethack screen.
+    ###
+
+    # hack in my 'inventory' tag to the last line.
+    scr_data[-1] = scr_data[-1].rstrip('\n') + " [I]\n"
+
+    htm_data = []
+
     # begin the <pre> section and draw the top frame. 
     top_str = '<pre>' + '&#9484;' + ('&#9472;' * 80 ) + '&#9488;'
     htm_data.append(top_str)
-    
+  
     # take the special-case status line
     status_str = scr_data[0].rstrip('\n')
     status_str = status_str.ljust(80, " ")
@@ -122,16 +101,16 @@ def build_page(turn=1, date="January 1, 2013"):
     
     # convert the list of strings to a single string.
     # keep header, body, footer separate. 
-    htm_header = "\n".join(htm_data[0:4])
-    htm_body = "\n".join(htm_data[4:-3])
+    htm_header = "\n".join(htm_data[0:2])
+    htm_body = "\n".join(htm_data[2:-3])
     htm_footer = "\n".join(htm_data[-3:])
 
-    # add in the inventory and object descriptions
-    with open('%s.inv' % turn) as inv_file:
-        inv_data = inv_file.readlines()
-    
-    with open('%s.obj' % turn) as obj_file: 
-        obj_data = obj_file.readlines()
+    ###
+    # Convert all characters to html-safe, and add object descriptions.
+    ###
+
+    inv_data = inv
+    obj_data = obj
 
     full_htm_data = ""
     full_htm_data += htm_header + '\n'
@@ -160,33 +139,63 @@ def build_page(turn=1, date="January 1, 2013"):
     inv_block = "".join(inv_data)        
     htm_footer = htm_footer.replace('[I]',"""<a href=# class="inv">[I]<div>%s</div></a>""" % inv_block)
 
-    # here's a sneaky trick. Make the alignment identifier a tag without a label to 
-    # 'clear' hover text on touch devices.
+    # here's a sneaky trick. Make the alignment identifier a tag
+    # without a label to 'clear' hover text on touch devices.
     # there *must* be a more elegant way to do this in python.
-    htm_footer = htm_footer.replace('Lawful', '<a href=# class="det">Lawful</a>')
-    htm_footer = htm_footer.replace('Neutral', '<a href=# class="det">Neutral</a>')
-    htm_footer = htm_footer.replace('Chaotic', '<a href=# class="det">Chaotic</a>')
+
+    htm_footer = htm_footer.replace('Lawful', 
+                                    '<a href=# class="det">Lawful</a>')
+    htm_footer = htm_footer.replace('Neutral', 
+                                    '<a href=# class="det">Neutral</a>')
+    htm_footer = htm_footer.replace('Chaotic',
+                                    '<a href=# class="det">Chaotic</a>')
 
     full_htm_data += "\n" + htm_footer
-    
-    with open('%s.str' % turn) as str_file:
-        str_data = str_file.read()
-    
-    with open('%s.tip' % turn) as tip_file:
-        tip_data = tip_file.read()
-    
-    html_footer = build_footer(story=str_data, tip=tip_data)
-                               
-    full_htm_data += "\n".join(html_footer)
-
     return full_htm_data
 
+def build_page(delve_id=0, turn=1, prev=0, next=2, date="January 1, 2013"):
+
+    delve_dir = cfg['delves'][0]['dir_name']
+
+    # import the screenshot data
+    with open(os.path.join(delve_dir, '%s.nss' % turn)) as scr_file:
+        scr_data = scr_file.readlines()
+
+    # add in the inventory and object descriptions
+    with open(os.path.join(delve_dir, '%s.inv' % turn)) as inv_file:
+        inv_data = inv_file.readlines()
+    
+    with open(os.path.join(delve_dir, '%s.obj' % turn)) as obj_file: 
+        obj_data = obj_file.readlines()
+
+    body = build_nethack_section(scr_data, obj_data, inv_data   )
+    
+    with open(os.path.join(delve_dir,'%s.str' % turn)) as str_file:
+        str_data = str_file.read()
+    
+    story = str_data
+    
+    with open(os.path.join(delve_dir,'%s.tip' % turn)) as tip_file:
+        tip_data = tip_file.read()
+    
+    tip = tip_data
+    
+    env = Environment(loader=FileSystemLoader(cfg['template_dir']))
+    template = env.get_template('game_state.html', globals=cfg)
+    
+    nav ='<ul class="nav"><li>First Turn</li><li>Previous Turn</li><li>Next Turn</li><li>Current Turn</li></ul>'
+    
+    return(template.render(date=date,
+                           turn=turn,
+                           entry=body,
+                           story=story,
+                           tip=tip,
+                           page_navigation=nav)) 
+
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: handle_url.py [turn number]")
+    if len(sys.argv) < 3:
+        print("Usage: handle_url.py [delve number] [turn number]")
     else:
-        turn = sys.argv[1]
-        #print("Processing data for turn %s" % turn)
-        
-        print(build_page(turn=turn))
-        #build_page(turn=turn)
+        delve = sys.argv[1]
+        turn = sys.argv[2]
+        print(build_page(delve_id=delve, turn=turn))
