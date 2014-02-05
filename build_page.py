3df5df9061fb3b9d4073bdfe761e63398bc7f7f9
Brian Fife <bdfife@gmail.com>
2014-02-05 08:45:05 -0500

    Added support for corpse character.

--- build_page.py
+++ build_page.py
@@ -136,7 +136,7 @@ def build_nethack_section(nss, obj, inv):
     full_htm_data += htm_header + '\n'
 
     # naturally, this will have to be filled out fully soon!
-    obj_list = "<@)+d=[k"
+    obj_list = "<@)+d=[k%"
     obj_counter = 0
 
     # replace an 'object of interest' with the marked up 
